5e712e976d7863fd4b8055041072bdacd06f375e
Jesus Campon <jesus.campon@gmail.com>
2018-01-16 00:37:46 +0000

    Renaming some project files

--- JCampon/MongoDB.Tests/Properties/AssemblyInfo.cs
+++ JCampon/MongoDB.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MongoDB.Tests")]
+[assembly: AssemblyTitle("JCampon.MongoDB.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MongoDB.Tests")]
+[assembly: AssemblyProduct("JCampon.MongoDB.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
